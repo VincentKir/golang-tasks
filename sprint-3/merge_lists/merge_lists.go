885f07e96d8729bdfe9e4277a6cf8f3662992057
vincent_kir <vincent_kir@mail.ru>
2025-12-21 17:18:14 +0300

    fix comments merge_lists

--- sprint-3/merge_lists/merge_lists.go
+++ sprint-3/merge_lists/merge_lists.go
@@ -5,18 +5,7 @@ type Node struct {
 	Next *Node
 }
 
-func compareNode(n1 *Node, n2 *Node) (*Node, *Node) {
-	if n1.Val < n2.Val {
-		return n1, n2
-	}
-	return n2, n1
-}
-
 func MergeLists(list1 *Node, list2 *Node) *Node {
-	if list1 == nil && list2 == nil {
-		return nil
-	}
-
 	if list1 == nil {
 		return list2
 	}
@@ -25,37 +14,51 @@ func MergeLists(list1 *Node, list2 *Node) *Node {
 		return list1
 	}
 
-	head, list := compareNode(list1, list2)
-	tmp_head := head.Next
-	tmp_list := list.Next
+	var mergeList *Node
 
-	prev_head := head
-	prev_list := list
+	if list1.Val < list2.Val {
+		mergeList = list1
+		list1 = list1.Next
 
-	for tmp_head != nil {
-		if prev_list != nil && tmp_head.Val >= prev_list.Val {
-			prev_head.Next = prev_list
-			prev_list.Next = tmp_head
-		}
-
-		if prev_list != nil && tmp_head.Val < prev_list.Val {
-			tmp_head.Next = prev_list
-			prev_head = tmp_head
-		}
-
-		if tmp_list != nil && tmp_head.Next == nil {
-			if tmp_head.Val < tmp_list.Val {
-				tmp_head.Next = tmp_list
-				tmp_list = tmp_head
-			}
-		}
-
-		prev_head = tmp_head
-		tmp_head = tmp_head.Next
-		prev_list = tmp_list
-		if tmp_list != nil {
-			tmp_list = tmp_list.Next
-		}
+	} else {
+		mergeList = list2
+		list2 = list2.Next
 	}
-	return head
+
+	nodeMerdgeList := mergeList
+
+	for list1 != nil || list2 != nil {
+
+		if list1 == nil {
+			nodeMerdgeList.Next = list2
+			list2 = list2.Next
+			nodeMerdgeList = nodeMerdgeList.Next
+			continue
+		}
+
+		if list2 == nil {
+			nodeMerdgeList.Next = list1
+			list1 = list1.Next
+			nodeMerdgeList = nodeMerdgeList.Next
+			continue
+		}
+
+		if list1.Val < list2.Val {
+			nodeMerdgeList.Next = list1
+			list1 = list1.Next
+			nodeMerdgeList = nodeMerdgeList.Next
+			continue
+
+		}
+
+		if list1.Val > list2.Val {
+			nodeMerdgeList.Next = list2
+			list2 = list2.Next
+			nodeMerdgeList = nodeMerdgeList.Next
+
+		}
+
+	}
+
+	return mergeList
 }
